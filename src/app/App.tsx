58d2c98e9d2dc0035a71cb4531ec01fc3331e032
Ulrich POKA <ulrich.poka.apps@email.com>
2025-12-14 20:31:25 +0100

    ...

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,40 +1,9 @@
-import reactLogo from '../assets/images/svg/react.svg';
-import { ENV } from '../constants';
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 export const App = () => {
 
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more {JSON.stringify(import.meta.env)}
-        Click on the Vite and React logos to learn more {ENV.name}
-      </p>
-      <p>
-        {ENV.name}
-      </p>
-    </>
+    <div>
+      Main page
+    </div>
   );
 
 };
