3752f048f2fe3b4fe682ecf8a83672a8f8912a2b
Ulrich POKA <ulrich.poka.apps@email.com>
2025-12-14 20:13:02 +0100

    ...

--- src/constants/env.constant.ts
+++ src/constants/env.constant.ts
@@ -1,3 +1,4 @@
 export const ENV = {
-  name: import.meta.env.VITE_ENVIRONMENT_NAME as ('development' | 'production' | 'test')
+  name: import.meta.env.VITE_ENVIRONMENT_NAME as ('development' | 'production' | 'test'),
+  baseApi: import.meta.env.VITE_BASE_API as string,
 };
