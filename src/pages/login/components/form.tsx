1fcc2f7ecaad6bccb98aaf98c6b4d0d8651e2947
Ulrich POKA <ulrich.poka.apps@email.com>
2025-12-18 00:22:00 +0100

    ...

--- src/pages/login/components/form.tsx
+++ src/pages/login/components/form.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/shadcn/button';
 import { Input } from '@/components/shadcn/input';
+import { useNavigate } from 'react-router-dom';
 import { cn } from '@/functions';
 import {
   FieldDescription,
@@ -13,8 +14,15 @@ import {
   Field,
   Card,
 } from '@/components/shadcn';
+import { ROUTES } from '@/constants';
 
 export const Form = () => {
+
+  const navigate = useNavigate();
+
+  const handleOnClick = () =>
+    navigate(ROUTES.home, { state: 12345 });
+
   return (
     <div className={cn('flex flex-col gap-6')}>
       <Card>
@@ -41,8 +49,7 @@ export const Form = () => {
                   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
                     <path
                       d='M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z'
-                      fill='currentColor'
-                    />
+                      fill='currentColor' />
                   </svg>
                   Login with Google
                 </Button>
@@ -56,23 +63,25 @@ export const Form = () => {
                   id='email'
                   type='email'
                   placeholder='m@example.com'
-                  required
-                />
+                  required />
               </Field>
               <Field>
                 <div className='flex items-center'>
                   <FieldLabel htmlFor='password'>Password</FieldLabel>
                   <a
                     href='#'
-                    className='ml-auto text-sm underline-offset-4 hover:underline'
-                  >
+                    className='ml-auto text-sm underline-offset-4 hover:underline'>
                     Forgot your password?
                   </a>
                 </div>
                 <Input id='password' type='password' required />
               </Field>
               <Field>
-                <Button type='submit'>Login</Button>
+                <Button
+                  type='button'
+                  onClick={handleOnClick}>
+                  Se connecter
+                </Button>
                 <FieldDescription className='text-center'>
                   Don&apos;t have an account? <a href='#'>Sign up</a>
                 </FieldDescription>
