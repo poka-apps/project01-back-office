a6a485eb4f2c9ea341e79b635a780a6688df3483
Ulrich POKA <ulrich.poka@technologypartner.io>
2026-01-02 16:11:42 +0100

    ...

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { HomePage, LoginPage, NotFoundPage, Page1, Page2 } from '@/pages';
+import { AnnoncesPage, HomePage, LoginPage, NotFoundPage, Page1, Page2, UsersPage } from '@/pages';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthLayout, MainLayout } from '@/layouts';
 import { ROUTES } from '@/constants';
@@ -9,6 +9,8 @@ export const AppRoutes = () => (
       {/* App with layout */}
       <Route element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path={ROUTES.annonces} element={<AnnoncesPage />} />
+        <Route path={ROUTES.users} element={<UsersPage />} />
         <Route path={ROUTES.page1} element={<Page1 />} />
         <Route path={ROUTES.page2} element={<Page2 />} />
       </Route>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,7 @@
 export * from './NotFound';
+export * from './annonces';
 export * from './Page1';
 export * from './Page2';
 export * from './login';
+export * from './users';
 export * from './Home';
