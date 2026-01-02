30e75dc932c7d4eedb52cfc384b9030d6fc86176
poka-apps <ulrich.poka.apps@gmail.com>
2026-01-02 21:38:22 +0100

    ...

--- src/pages/nomenclatures/NomenclaturesPage.tsx
+++ src/pages/nomenclatures/NomenclaturesPage.tsx
@@ -1,10 +1,21 @@
+import { Tabs, TabsList, TabsTrigger } from '@/components/shadcn';
+
 const NomenclaturesPage = () => {
 
   console.log('Page nomenclatures');
 
   return (
     <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-      Nomenclatures
+      <Tabs defaultValue='account'>
+        <TabsList className='cursor-pointer'>
+          <TabsTrigger value='account'>
+            Marques
+          </TabsTrigger>
+          <TabsTrigger value='password'>
+            Modèles
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
     </div>
   );
 
