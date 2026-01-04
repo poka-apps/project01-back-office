639232ae7624a345af1135cb6e4559ff9d7d1b0c
Ulrich POKA <ulrich.poka.apps@email.com>
2026-01-04 14:16:21 +0100

    ...

--- src/pages/nomenclatures/components/brands/BrandsDataTable.tsx
+++ src/pages/nomenclatures/components/brands/BrandsDataTable.tsx
@@ -1,35 +1,81 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/shadcn';
 import { type ColumnDef } from '@tanstack/react-table';
-import type { IHasId, IHasTitle } from '@/interfaces';
+import type { IBrand } from '@/interfaces';
 import { DataTable } from '@/components';
 import { nameof } from '@/functions';
 
-export const columns: ColumnDef<IHasId & IHasTitle<string>>[] = [
+export const columns: ColumnDef<IBrand>[] = [
   {
-    accessorKey: nameof<IHasId>('id'),
+    accessorKey: nameof<IBrand>('id'),
     header: 'Id'
   },
   {
-    accessorKey: nameof<IHasTitle>('title'),
+    accessorKey: nameof<IBrand>('title'),
     header: 'Intitulé'
   }
 ];
 
 export const BrandsDataTable = () => {
 
+  const data = [
+    {
+      id: '1',
+      title: 'Element 1'
+    },
+    {
+      id: '2',
+      title: 'Element 2'
+    }
+  ] as IBrand[];
+
+  return (
+    <div className='overflow-hidden rounded-md border'>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>
+              Id
+            </TableHead>
+            <TableHead>
+              Intitulé
+            </TableHead>
+            <TableHead>
+              Crée le
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+            data
+              .map(
+                l => (
+                  <TableRow key={l.id}>
+                    <TableCell>
+                      {l.id}
+                    </TableCell>
+                    <TableCell>
+                      {l.title}
+                    </TableCell>
+                    <TableCell>
+                      {l.createdAt?.toString()}
+                    </TableCell>
+                  </TableRow>
+                )
+              )
+          }
+          <TableRow>
+
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
+
   return (
     <div>
       <DataTable
         columns={columns}
-        data={[
-          {
-            id: '1',
-            title: 'Element 1'
-          },
-          {
-            id: '2',
-            title: 'Element 2'
-          }
-        ]} />
+        data={data} />
     </div>
   );
 
