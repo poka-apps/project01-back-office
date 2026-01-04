56d7d9a520d70e5826d42e23b24c81254c376735
Ulrich POKA <ulrich.poka.apps@email.com>
2026-01-04 19:41:14 +0100

    ...

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,7 +1,16 @@
-import { LazyAnnoncesPage, LazyGaragesPage, LazyDashboardPage, LazyLoginPage, LazyNotFoundPage, LazyUsersPage, LazyNomenclaturesPage } from '@/pages';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthLayout, MainLayout } from '@/layouts';
 import { ROUTES } from '@/constants';
+import {
+  LazyNomenclaturesPage,
+  LazyEquipmentsPage,
+  LazyDashboardPage,
+  LazyAnnoncesPage,
+  LazyNotFoundPage,
+  LazyGaragesPage,
+  LazyLoginPage,
+  LazyUsersPage
+} from '@/pages';
 
 export const AppRoutes = () => (
   <BrowserRouter>
@@ -9,8 +18,10 @@ export const AppRoutes = () => (
       {/* App with layout */}
       <Route element={<MainLayout />}>
         <Route index element={<LazyDashboardPage />} />
+        {/* #region nomenclatures */}
+        <Route path={ROUTES.nomenclatures.equipments} element={<LazyEquipmentsPage />} />
         <Route path={ROUTES.nomenclatures.brands} element={<LazyNomenclaturesPage />} />
-        <Route path={ROUTES.nomenclatures.equipments} element={<></>} />
+        {/* #endregion */}
         <Route path={ROUTES.annonces} element={<LazyAnnoncesPage />} />
         <Route path={ROUTES.garages} element={<LazyGaragesPage />} />
         <Route path={ROUTES.users} element={<LazyUsersPage />} />
--- src/pages/nomenclatures/index.ts
+++ src/pages/nomenclatures/index.ts
@@ -1,1 +1,2 @@
-export * from './LazyNomenclaturesPage';
+export * from './equipments';
+export * from './brands';
